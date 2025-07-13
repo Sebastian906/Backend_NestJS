d63e0fa45002365fd30b9ac9c3abe4d285fbf310
Sebastian906 <elsebas1912@gmail.com>
2025-07-13 16:33:04 -0500

    Configuración en controlador para visualizar petición de GET

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Get()
-  findAll() {
-    return this.productService.findAll();
+  @Get('/:productId/:userId')
+  findAll(
+    @Param('productId', ParseIntPipe) id: string,
+    @Param('userId') userId: string
+  ) {
+    return {id, userId};
   }
 }
